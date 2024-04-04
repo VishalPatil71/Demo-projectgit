3ee72e1c6c254195951c5eada93db23ab809fdab
vishalspatil71@gmail.com <vishalspatil71@gmail.com>
2024-04-04 13:28:02 +0530

    first commit

---
+++ Demogit/src/main/java/Projectjava/Test1.java
@@ -0,0 +1,22 @@
+package Projectjava;
+
+public class Test1 {
+	
+ 
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			int a=100;
+			int b=200;
+			int temp;
+			 
+			temp=a;
+		    a=b;
+		    b=temp;
+		    System.out.println("a-->"+a+"b-->"+b);
+
+		}
+
+	}
+
+ 
