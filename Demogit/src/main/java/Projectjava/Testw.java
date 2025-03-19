0d3f02ff7bb837dcbce0ac8092a2039ae1a8bd62
Vishal Patil <119045423+VishalPatil71@users.noreply.github.com>
2025-03-19 14:01:42 +0530

    first commit

---
+++ Demogit/src/main/java/Projectjava/Testw.java
@@ -0,0 +1,12 @@
+package Projectjava;
+
+public class Testw {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Test 2 create");
+
+	}
+
+}
